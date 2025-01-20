b992bec6e54bb6b5161e854ad95a513767cde17e
Alisson Vargas <contato@alissonsv.com>
2025-01-19 21:47:37 -0300

    chore: remove duplicated test and log

--- tests/presentation/http/pets/create-pet-route.test.ts
+++ tests/presentation/http/pets/create-pet-route.test.ts
@@ -2,7 +2,6 @@ import { randomUUID } from "node:crypto";
 import { faker } from "@faker-js/faker/.";
 import request from "supertest";
 import { app } from "#/app";
-import { prismaClient } from "#/lib/prisma";
 import { createAndAuthenticateOrg } from "#/tests/utils/test/create-and-authenticate-org";
 
 describe("Create Pet (e2e)", () => {
@@ -16,8 +15,6 @@ describe("Create Pet (e2e)", () => {
 
 	test("Should be able to create a new pet", async () => {
 		const { token } = await createAndAuthenticateOrg(app);
-
-		console.log(token);
 
 		await request(app.server)
 			.post("/pets")
@@ -33,40 +30,5 @@ describe("Create Pet (e2e)", () => {
 				orgId: randomUUID(),
 			})
 			.expect(201);
-
-		await prismaClient.org.deleteMany();
-	});
-
-	test("Should return 409 if email is already taken", async () => {
-		const email = faker.internet.email();
-
-		await request(app.server)
-			.post("/orgs")
-			.send({
-				name: faker.word.words(2),
-				email,
-				password: faker.internet.password(),
-				managerName: faker.person.fullName(),
-				whatsapp: faker.phone.number(),
-				address: faker.location.streetAddress(),
-				cep: faker.location.zipCode("########"),
-				city: faker.location.city(),
-			});
-
-		await request(app.server)
-			.post("/orgs")
-			.send({
-				name: faker.word.words(2),
-				email,
-				password: faker.internet.password(),
-				managerName: faker.person.fullName(),
-				whatsapp: faker.phone.number(),
-				address: faker.location.streetAddress(),
-				cep: faker.location.zipCode("########"),
-				city: faker.location.city(),
-			})
-			.expect(409);
-
-		await prismaClient.org.deleteMany();
 	});
 });
